b115bea1700a2b138c586b71427bdfe2f67fd5fc
Artem Solod <artemsolod@Artems-MacBook-Pro.local>
2014-04-26 23:15:11 +0400

    create_hosted request support

--- cryptopay_api.py
+++ cryptopay_api.py
@@ -1,28 +1,28 @@
 from __future__ import print_function
 import httplib
 from urllib import urlencode
-from json import loads
+from json import loads, dumps
 
 
 
-YOUR_API_KEY = "c91336d748a4f1dc6589c1653bf6be37"
+YOUR_API_KEY = ""
 
 url = "cryptopay.me"
 
-def get_balance():
-	return basic_request("/balance", with_token=True)
-
 def basic_request(req_path, args_dict={}, with_token=False, req_type="GET"):
-	default_headers={'Content-Type': 'application/x-www-form-urlencoded'}
+	default_headers={'Content-Type': 'application/json'}
 	h = httplib.HTTPSConnection(url)
-	if with_token:
-		args_dict["api_key"] = YOUR_API_KEY
-	h.request(req_type, "/api/v1" + req_path, urlencode(args_dict), default_headers)
+	total_path = "/api/v1" + req_path + (("?api_key=" + YOUR_API_KEY) if with_token else "")
+	h.request(req_type, total_path, dumps(args_dict), default_headers)
 	response = h.getresponse()
 	if (200 <= response.status < 300):
 		return loads(response.read())
 	else:
-		return "Server Error Code: " + str(response.status)
+		return "Server Error Code: " + str(response.read()) # str(response.status)
+
+
+def get_balance():
+	return basic_request("/balance", with_token=True)
 
 def create_invoice(price, **kwargs):
 	""" Optional arguments and their defaults are
@@ -43,11 +43,11 @@ def requote(uuid):
 def list_invoices(page_number=1, per_page=20, from_time=None, to_time=None):
 	args_dict= {}
 	args_dict['per_page'] = per_page
-	args_dict['page_number'] = page_number
-	args_dict['from'] = from_time
-	args_dict['to_time'] = to_time
-	req_path = "/invoices/"
-	return basic_request(req_path, with_token=True, req_type="GET")
+	args_dict['page'] = page_number
+	if from_time: args_dict['from'] = from_time
+	if to_time: args_dict['to'] = to_time
+	req_path = "/invoices"
+	return basic_request(req_path, args_dict, True, "GET")
 
 def create_button(price, currency=None, name=None):
 	args_dict = {}
@@ -57,7 +57,13 @@ def create_button(price, currency=None, name=None):
 	req_path = "/buttons"
 	return basic_request(req_path, args_dict, True, "POST")
 
+def create_hosted(items, **kwargs):
+	kwargs['items'] = items
+	req_path = "/hosted"
+	return basic_request(req_path, kwargs, True, "POST")
+
 
 if __name__ == "__main__":
-	res = create_button(100, "GBP", "lol")
+	its = [{"name": "lol", "price": 50, "quantity": 2, "vat_rate":10}]
+	res = create_hosted(its)
 	print(res)
